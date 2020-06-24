fdd69495d1768b4b8736dbc862e252436e588a45
self-affected <ihnatov.d@gmail.com>
2020-06-24 21:05:18 +0300

    Added publisher

--- src/main/java/co/winish/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/co/winish/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package co.winish.spring5webapp.bootstrap;
 
 import co.winish.spring5webapp.domain.Author;
 import co.winish.spring5webapp.domain.Book;
+import co.winish.spring5webapp.domain.Publisher;
 import co.winish.spring5webapp.repositories.AuthorRepository;
 import co.winish.spring5webapp.repositories.BookRepository;
+import co.winish.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,19 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher james = new Publisher("James Hetfield", "Playboy mansion, 1");
+        publisherRepository.save(james);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
@@ -39,5 +46,6 @@ public class BootstrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of book: " + bookRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
     }
 }
---
+++ src/main/java/co/winish/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package co.winish.spring5webapp.repositories;
+
+import co.winish.spring5webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
